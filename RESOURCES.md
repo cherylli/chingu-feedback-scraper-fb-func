56e7b6cee76ae62de3f989bb8a556e3002b43006
Cheryl M <webmaster@cherylli.com>
2023-10-01 19:49:53 +1100

    add functionality to save the feedback json file on firebase store

--- RESOURCES.md
+++ RESOURCES.md
@@ -7,3 +7,10 @@ https://hume.dev/articles/puppeteer-google-cloud-functions-v2/
 Also need to downgrade puppeteer to 18.2.1
 https://stackoverflow.com/questions/74089894/deploying-firebase-function-with-puppeteer-says-chrome-cant-be-found-even-thoou
 
+## Storage bucket
+https://medium.com/@dorathedev/uploading-json-objects-as-json-files-to-firebase-storage-without-having-or-creating-a-json-file-38ad323af3c4
+
+https://www.youtube.com/watch?v=qZ1EFnFOGvE&ab_channel=Academind
+
+## General
+https://www.youtube.com/watch?v=d25dg0AgkwQ&ab_channel=CodewithSloba
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,37 +1,29 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import {onRequest} from "firebase-functions/v2/https";
+import {initializeApp} from "firebase/app";
 import {runScraper} from "./scraper";
+import {getStorage, ref, uploadString} from "firebase/storage";
+
+
+const firebaseConfig = {
+  storageBucket: process.env.STORAGEBUCKET,
+};
+
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
 
 exports.scrape = onRequest({
   memory: "512MiB",
   region: "australia-southeast1",
 }, async (req, res) => {
   const feedback = await runScraper();
+
+  const jsonFile = JSON.stringify(feedback);
+  const feedbackRef = ref(storage, "feedback.json");
+  uploadString(feedbackRef, jsonFile).then((snapshot)=>{
+    console.log(snapshot);
+  });
   res.send(feedback);
 });
-/*
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const functions = require("firebase-functions");
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const scrapeGithub = require("./scraper");
-import {Request, Response} from "express";
 
-exports.scrape = functions
-  .runWith({
-    timeoutSeconds: 120,
-    memory: "512MB" || "2GB",
-  }).region("australia-southeast1")
-  .https.onRequest( async (req:Request, res:Response) => {
-    const feedback = await scrapeGithub();
-    console.log(feedback);
-    res.send(feedback);
-  });
-*/
